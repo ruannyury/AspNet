8e5c35dac2b63f43702859c77c8ad5cecef8abf1
ruannyury <ruannyury1@outlook.com>
2022-03-15 01:00:34 -0300

    Tentativa de API

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+        
+        public DbSet<Product> Products { get; set; }
+        
+        public DbSet<Category> Categories { get; set; }
+    }
+}
